8817a48848a3bba304e7d043e5660cd3850c384f
Ricky <f3nrir.d@gmail.com>
2022-09-14 15:23:20 +0200

    add Markdown dependecie and Internet Permision

--- app/src/main/java/be/bf/kit3tsu/connect_the_books/MainActivity.kt
+++ app/src/main/java/be/bf/kit3tsu/connect_the_books/MainActivity.kt
@@ -3,14 +3,21 @@ package be.bf.kit3tsu.connect_the_books
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import be.bf.kit3tsu.connect_the_books.ui.theme.ConnectTheBooksTheme
+import dev.jeziellago.compose.markdowntext.MarkdownText
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +29,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column() {
+                        Greeting("Android")
+                        ComplexExampleContent()
+                        MinimalExampleContent()
+                    }
                 }
             }
         }
@@ -34,10 +45,44 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+//Minimal example
+@Composable
+fun MinimalExampleContent() {
+    MarkdownText(markdown = """  
+	# Sample  
+    ## Hé title
+	* Markdown  
+	* [Link](https://example.com)  
+	![Image](https://example.com/img.png)  
+	<a href="https://www.google.com/">Google</a>  
+""")
+}
+ val test = "  # Sample \n" +
+        // "## Hé title\n" +
+         " <a href=\"https://www.google.com/\">Google</a>    \n"
+//Complex example
+@Composable
+fun ComplexExampleContent() {
+    MarkdownText(
+        modifier = Modifier.padding(8.dp),
+        markdown = test,
+        textAlign = TextAlign.Center,
+        fontSize = 12.sp,
+        color = LocalContentColor.current,
+        maxLines = 3,
+        style = MaterialTheme.typography.overline,
+        )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ConnectTheBooksTheme {
-        Greeting("Android")
+        Column() {
+            Greeting("Android")
+            MinimalExampleContent()
+            ComplexExampleContent()
+        }
+
     }
 }
